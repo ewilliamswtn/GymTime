c0637c3cbc0c1dd27d4d03b89a7f7668d33f13e1
Elliott <ewilliamswtn@gmail.com>
2016-09-17 16:41:35 -0500

    another bad commit.  made progress in deploy feature

--- app/controllers/routineDeployCtrl.js
+++ app/controllers/routineDeployCtrl.js
@@ -1,12 +1,18 @@
 "use strict";
 
 app.controller("routineDeployCtrl", function ($scope, $window, authFactory, userFactory, routineFactory, exerciseFactory) {
-
+$scope.$watch('ready', function() {
+  if($scope.ready == true) {
+    //now the data-id attribute works
+  }
+});
   //array of exercise keys array, made by splitting the string of exercise keys located in $scope.routine.exercises
   $scope.exerciseKeyArray = [];
 
   //array of exercise objects, populated by using $scope.exerciseKeyArray to make firebase calls for each exercise key
   $scope.exerciseObjArray = [];
+
+  $scope.doneExerciseKeyArray = [];
 
   //get data for the selected routine via firebase
   routineFactory.getRoutine($scope.$parent.selectedRoutine)  //selected routine is a key, used to get a full object from firebase
@@ -27,5 +33,29 @@ app.controller("routineDeployCtrl", function ($scope, $window, authFactory, user
       }); //end .then following getExercise(s) call
     }); //end looper over exerciseArray
   }); //end .then following getRoutine call
+
+  $scope.allDone = function  () {
+    $(".doneCB").prop("checked", true);
+  }
+
+  $scope.finish = function () {
+    $.each($('input:checked'), function (index, value) {
+      $scope.doneExerciseKeyArray.push(value.id);
+    });
+
+    $.each($scope.exerciseObjArray, function (index, value) {
+      console.log(index, value);
+    })
+
+    let d = new Date();
+    let day = d.getDate() + 1;
+    let month = d.getMonth() + 1;
+    let year = d.getFullYear();
+    let date = day + " / " +  month + " / " + year;
+    console.log(date);
+
+
+  }
+
 
 });
